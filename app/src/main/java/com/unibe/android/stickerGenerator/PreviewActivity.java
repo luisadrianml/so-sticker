f89a9afe96132a2c4d09d55ee034a7436b1fe3cc
Fernando Perez <0330.fernando@gmail.com>
2015-04-15 22:59:31 -0400

    add library for pdf generation
    adapt the pdf generation

--- app/src/main/java/com/unibe/android/stickerGenerator/PreviewActivity.java
+++ app/src/main/java/com/unibe/android/stickerGenerator/PreviewActivity.java
@@ -1,5 +1,9 @@
 package com.unibe.android.stickerGenerator;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.onbarcode.barcode.android.AndroidColor;
 import com.onbarcode.barcode.android.AndroidFont;
 import com.onbarcode.barcode.android.Code11;
@@ -17,6 +21,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,12 +29,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class PreviewActivity extends Activity {
 	
 	public static final String EXTRA_PRODUCT = "ext_product";
-	
+
+
+    private Product p = null;
+    private Bitmap bmBarcode = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +46,14 @@ public class PreviewActivity extends Activity {
 		setContentView(R.layout.activity_preview);
 		
 		Intent i = getIntent();
-		Product p = (Product) i.getSerializableExtra(EXTRA_PRODUCT);
+		p = (Product) i.getSerializableExtra(EXTRA_PRODUCT);
 		
 		TextView tvProductCode = (TextView) findViewById(R.id.tvProductCode);
 		TextView tvProductName= (TextView) findViewById(R.id.tvProductName);
 		
 		
 		ImageView imgBarcode = (ImageView) findViewById(R.id.imgvBarcode);
-		Bitmap bmBarcode = Bitmap.createBitmap(500, 200, Bitmap.Config.ARGB_8888); 
+		bmBarcode = Bitmap.createBitmap(500, 200, Bitmap.Config.ARGB_8888);
 		Canvas canvasBarcode = new Canvas(bmBarcode);
 		try {
 			generateBarCode(p, canvasBarcode);
@@ -110,14 +119,41 @@ public class PreviewActivity extends Activity {
 	 * @since 1.0
 	 */
 	public void onClickPrint(View view) throws IOException {
-		//TODO: finish this module with Google Cloud Print
         if (!isNetworkAvailable()) {
             Toast.makeText(PreviewActivity.this,
                     getResources().getString(R.string.notNetwork),
                     Toast.LENGTH_SHORT).show();
         } else {
+            boolean error = false;
 
-            File file = new File("file:///android_asset/silabo.pdf");
+            String path_pdf_temp = getApplicationContext().getCacheDir() + "/sticker.pdf";
+
+            //generate pdf to print
+            try{
+
+                Document doc = new Document();
+                Paragraph par;
+                PdfWriter.getInstance(doc, new FileOutputStream(path_pdf_temp));
+                doc.open();
+
+
+
+                par = new Paragraph(50);
+                par.add(getString(R.string.product_code) + ": " + p.getId());
+                doc.add(par);
+                par = new Paragraph(50);
+                par.add(getString(R.string.product_name) + ": " + p.getName());
+                doc.add(par);
+
+                doc.close();
+
+            }catch(Exception e){
+                error = true;
+            }
+            //end: generate pdf to print
+
+
+            File file = new File(path_pdf_temp);
             Intent printIntent = new Intent(PreviewActivity.this, PrintDialogActivity.class);
             printIntent.setDataAndType(Uri.fromFile(file), "application/pdf");
             printIntent.putExtra(PrintDialogActivity.PRINT_FLAG_EXTRA, "Android print demo");
